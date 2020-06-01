945f410fc680cffcf3f2b2dd030d9ab283af7586
captain-pool <rickdey1998@gmail.com>
2020-06-01 19:19:48 +0530

    Fixin Minor Bugs

--- launch_server.sh
+++ launch_server.sh
@@ -1,1 +1,4 @@
-nohup python3 -u discretion.py -m en_core_web_sm -cfg configs/*yaml -ctx $1 -p 8080 &> stdout.log &
+ctx_name="$1"
+shift
+python3 -u discretion.py -m en_core_web_sm -cfg configs/*yaml -ctx $ctx_name -p 8080 "$@" &> stdout.log &
+echo "PID: $!"
